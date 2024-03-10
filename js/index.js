6c8e94a6e3a938ebedd4d1c01af98772c06cfdac
Mohammadreza Saeedi <m.r.saeedi1995@gmail.com>
2024-03-10 21:21:43 +0330

    add:addinputtitle js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,26 @@
 const buttonAddBuckets = document.getElementById("add_buckets")
 const titel = document.getElementById("titel_of_buckets")
+const addInput =document.getElementById("input_titel")
+const svgAddInput= document.getElementById("svg_add_titel")
 
+let titels = JSON.parse(localStorage.getItem("titels"));
 const addBucketsHandler = () => {
     buttonAddBuckets.style.display="none"
     titel.style.display="block"
-
+    
 }
-
-
-
+const addTitelHandler = () => {
+    const task =addInput.value;
+    const titel = {
+        tasktitel:task;
+    }
+    if(titel){
+        titels.push(titel)
+        addInput.value ="";
+    }
+}
+const showalert = () => {
+    
+}
+svgAddInput.addEventListener("click",addTitelHandler)
 buttonAddBuckets.addEventListener("click",addBucketsHandler)

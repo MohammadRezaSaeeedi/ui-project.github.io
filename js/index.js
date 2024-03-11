b06a4099243407b1d5034864cb48a51f8b31fc49
Mohammadreza Saeedi <m.r.saeedi1995@gmail.com>
2024-03-11 22:25:10 +0330

    title edit

--- js/index.js
+++ js/index.js
@@ -1,26 +1,54 @@
-const buttonAddBuckets = document.getElementById("add_buckets")
-const titel = document.getElementById("titel_of_buckets")
-const addInput =document.getElementById("input_titel")
-const svgAddInput= document.getElementById("svg_add_titel")
+const buttonAddBuckets = document.getElementById("add_buckets");
+const title = document.getElementById("title_of_buckets");
+const addInput = document.getElementById("input_title");
+const svgAddInput = document.getElementById("svg_add_title");
+const alertMessage = document.getElementById("alert_message");
+const buckets = document.getElementById("buckets");
 
-let titels = JSON.parse(localStorage.getItem("titels"));
+let titles = JSON.parse(localStorage.getItem("titles")) || [];
+
+const generateId = () => {
+  return Math.round(
+    Math.random() * Math.random() * Math.pow(12, 13)
+  ).toString();
+};
+const saveToLocalStorage = () => {
+  localStorage.setItem("titles", JSON.stringify(titles));
+};
+
 const addBucketsHandler = () => {
-    buttonAddBuckets.style.display="none"
-    titel.style.display="block"
-    
-}
-const addTitelHandler = () => {
-    const task =addInput.value;
-    const titel = {
-        tasktitel:task;
-    }
-    if(titel){
-        titels.push(titel)
-        addInput.value ="";
-    }
-}
-const showalert = () => {
-    
-}
-svgAddInput.addEventListener("click",addTitelHandler)
-buttonAddBuckets.addEventListener("click",addBucketsHandler)
+  buttonAddBuckets.style.display = "none";
+  title.style.display = "block";
+};
+const addtitleHandler = () => {
+  const task = addInput.value;
+  const title = {
+    id: generateId(),
+    tasktitle: task,
+  };
+  if (title.tasktitle !== "" ) {
+    titles.push(title);
+    saveToLocalStorage();
+    addInput.value = "";
+    // title.style.display = "none"
+    // buckets.style.display = "block";
+
+    showalert("title added successfully", "success");
+  } else {
+    showalert("please enter a title!", "error");
+  }
+};
+const showalert = (message, type) => {
+  alertMessage.innerHTML = "";
+  const alert = document.createElement("p");
+  alert.innerText = message;
+  alert.classList.add("alert");
+  alert.classList.add(`alert_${type}`);
+  alertMessage.append(alert);
+  setTimeout(() => {
+    alert.style.display = "none";
+  }, 2000);
+};
+
+svgAddInput.addEventListener("click", addtitleHandler);
+buttonAddBuckets.addEventListener("click", addBucketsHandler);
